b53c790d0d41433f57e3e1766d75d232b30ae4bd
Dikshant2994 <dikm.0694@gmail.com>
2025-07-03 21:03:53 +0530

    Add files via upload

---
+++ preprocessor.py
@@ -0,0 +1,57 @@
+import re
+import pandas as pd
+
+def preprocess(data):
+    pattern = r"\d{1,2}/\d{1,2}/\d{2},\s\d{1,2}:\d{2}\u202f[AP]M\s-\s"
+    messeges = re.split(pattern, data)[1:]
+    dates = re.findall(pattern, data)
+    print("Messages:", len(messeges))
+    print("Dates   :", len(dates))
+    min_len = min(len(messeges), len(dates))
+    df = pd.DataFrame({
+        'user_messages': messeges[:min_len],
+        'messages_date': dates[:min_len]})
+
+    df['messages_date'] = df['messages_date'].str.replace('\u202f', ' ', regex=True)
+    df['messages_date'] = pd.to_datetime(df['messages_date'].str.replace(' - ', ''), format='%m/%d/%y, %I:%M %p')
+
+    df.rename(columns={'messages_date': 'date'}, inplace=True)
+
+    users = []
+    message_texts = []
+
+    for messages in df['user_messages']:
+        entry = re.split(r'([\w\s]+):\s', messages)
+        if entry[1:]:
+            users.append(entry[1])
+            message_texts.append(entry[2])
+        else:
+            users.append('group_notification')
+            message_texts.append(entry[0])
+
+    df['user'] = users
+    df['message'] = message_texts
+
+    df.drop(columns=['user_messages'], inplace=True)
+
+    df['year'] = df['date'].dt.year
+    df['month_num'] = df['date'].dt.month
+    df['month'] = df['date'].dt.month_name()
+    df['day'] = df['date'].dt.day
+    df['only_date'] = df['date'].dt.date
+    df['day_name'] = df['date'].dt.day_name()
+    df['hour'] = df['date'].dt.hour
+    df['minute'] = df['date'].dt.minute
+
+    period = []
+    for hour in df[['day_name', 'hour']]['hour']:
+        if hour == 23:
+            period.append(str(hour) + '-' + str('00'))
+        elif hour == 0:
+            period.append(str('00') + '-' + str(hour + 1))
+        else:
+            period.append(str(hour) + '-' + str(hour + 1))
+
+    df['period'] = period
+
+    return df
